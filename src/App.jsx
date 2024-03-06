6f419a7fc4e80036a1f7c2535dc5656fa7ef7209
Wizardy <wizardhassan99@gmail.com>
2024-03-06 23:06:56 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import NavBar from "./components/main-components/nav-bar/NavBar";
+import Footer from "./components/main-components/footer/Footer";
+import Accueil from "./pages/accueil/Accueil";
+import Contact from "./pages/contact/Contact";
+import NosProgrammes from "./pages/nos-programmes/NosProgrammes";
+import NosProjets from "./pages/nos-projets/NosProjets";
+import QuiSommeNous from "./pages/qui-some-nous/QuiSommeNous";
 import './App.scss'
 
-function App() {
-
+const Layout = () => {
   return (
     <>
-      <h1>Fondation Abdelouahed EL KADIRI</h1>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
     </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/", element:<Layout/>, children: [
+      { path:"/", element: <Accueil /> },
+      { path:"/contact", element: <Contact /> },
+      { path:"/nos-programmes", element: <NosProgrammes /> },
+      { path:"/nos-projets", element: <NosProjets /> },
+      { path:"/qui-somme-nous", element: <QuiSommeNous /> },
+    ],
+  }
+])
+
+function App() {
+  return (
+    <div className="app">
+    <div className="container">
+      <RouterProvider router={router}/>
+    </div>
+  </div>
   )
 }
 
