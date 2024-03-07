0a3175643505b08970e2238cfcea020add121cf6
Wizardy <wizardhassan99@gmail.com>
2024-03-07 22:17:47 +0100

    commit

--- src/pages/qui-some-nous/QuiSommeNous.jsx
+++ src/pages/qui-some-nous/QuiSommeNous.jsx
@@ -1,10 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './qui-somme-nous.scss'
+import React from 'react';
+import './qui-somme-nous.scss';
 
 function QuiSommeNous() {
   return (
-    <div>QuiSommeNous</div>
+    <div className="qui-some-nous">
+      <div className="mot-president">
+        <div className="left">
+          <img src="\uploads\mot-president-kadiri.jpg" alt="Abdelouahed EL KADIRI" />
+        </div>
+        <div className="right">
+          <p>
+            Mes remercîments les plus sincères vont aux acteurs qui se mobilisent et qui permettent à la Fondation Abdelouahed EL KADIRI, depuis sa création en 2007 puis sa reconnaissance d'utilité publique en 2012, de continuer à amplifier son action en faveur de l'éducation, la culture, l'art, le sport, la recherche et une multitude d'actions caritatives auxquels s'ajoute la construction du centre culturel multidisciplinaire qui abrite une bibliothèque, salles de cinéma, salles d'expositions des tableaux d'art, salles des cours de peinture et de formation artistique...etc.
+          </p>
+          <p>
+            Nos programmes reposent sur les trois grands axes de la feuille de route de la fondation à savoir : <br />
+            <span> - Le soutien de projets caritatifs, éducatifs et culturels</span><br />
+            <span> - La promotion de l'art et le soutien des artistes</span><br />
+            <span> - La promotion du sport</span><br />
+            <span> - La valorisation de la recherche et du développement</span><br />
+            <p>
+            Cette feuille de route est le fruit d'un travail constant de notre Conseil d'Administration qui se réunit régulièrement pour décider du financement des projets retenus. Car, au-delà des sommes que nous versons, nous nous impliquons auprès des porteurs de projets pour les aider à trouver ensembles les meilleures solutions. Etre une « caution morale », un facilitateur fait aussi partie de nos missions...
+            </p>
+            <p>
+            En quête de solutions toujours plus innovantes et efficaces pour le développement de la santé, de l'éducation, de la culture et du sport, notre équipe continue à repérer les acteurs qui développent des projets innovants et de qualité au profit des personnes les plus nécessiteuses.
+            </p>
+            <p>
+            Nous restons également très impliqués auprès des chercheurs pour aider à faire émerger de nouvelles réponses dans le domaine de l'éducation, à travers notre prix « Abdelouahed EL KADIRI ».
+            </p>
+          </p>
+        </div>
+      </div>
+      <div className="notre-histoire">
+        <h2>NOTRE HISTOIRE</h2>
+      </div>
+      <div className="a-propos">
+        <h1>
+        a propos de nous
+        </h1>
+      </div>
+    </div>
   )
 }
 
