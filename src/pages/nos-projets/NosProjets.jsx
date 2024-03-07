2f70a5c0fe99e27422e5febf28352c7329dbc4bb
Wizardy <wizardhassan99@gmail.com>
2024-03-08 00:27:00 +0100

    commit

--- src/pages/nos-projets/NosProjets.jsx
+++ src/pages/nos-projets/NosProjets.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './nos-projets.scss'
+import Bibliotheque from '../../components/projets-components/bibliotheque/Bibliotheque'
+import SallesCinema from '../../components/projets-components/salles-cinema/SallesCinema'
+import AteliersMusiquePeitures from '../../components/projets-components/ateliers-musique-peiture/AteliersMusiquePeitures'
+import GalerieExposition from '../../components/projets-components/galerie-exposition/GalerieExposition'
+import Auditorium from '../../components/projets-components/auditorium/Auditorium'
 
 function NosProjets() {
   return (
-    <div>NosProjets</div>
+    <div className="nos-projets">
+      <Bibliotheque/>
+      <SallesCinema/>
+      <AteliersMusiquePeitures/>
+      <GalerieExposition/>
+      <Auditorium/>
+    </div>
   )
 }
 
