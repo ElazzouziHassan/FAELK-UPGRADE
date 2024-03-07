b2126e3ffa5e0b4afdfc7af131a8d08a47a82efb
Wizardy <wizardhassan99@gmail.com>
2024-03-08 00:26:45 +0100

    commit

--- src/components/projets-components/galerie-exposition/GalerieExposition.jsx
+++ src/components/projets-components/galerie-exposition/GalerieExposition.jsx
@@ -4,7 +4,21 @@ import './galerie-exposition.scss'
 
 function GalerieExposition() {
   return (
-    <div>GalerieExposition</div>
+    <div className="galerie-exposition">
+      <div className="left">
+        <h2>Galerie d'exposition</h2>
+          <hr />
+          <p>
+            A travers ses expositions, la Fondation s’engage à promouvoir la création artistique moderne et contemporaine et contribue à son rayonnement tant sur le plan national qu’international.
+          </p>
+          <p>
+            Chaque année des expositions temporaires d’art moderne et d’art contemporain marquent les temps forts de la Fondation, des expositions qui s’adressent à tous.
+          </p>
+      </div>
+      <div className="right">
+        <img src="\uploads\galerie-exposition.jpg" alt="Galerie d'exposition" />
+      </div>
+    </div>
   )
 }
 
