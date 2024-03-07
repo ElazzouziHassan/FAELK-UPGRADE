1b4f7972f303ffb5c42d00146cc7e5df47cdb927
Wizardy <wizardhassan99@gmail.com>
2024-03-07 13:08:30 +0100

    commit

--- src/components/main-components/nav-bar/NavBar.jsx
+++ src/components/main-components/nav-bar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     <div className="nav-bar">
       <div className="logo">
-        <a Link="/"><img src="public\uploads\logo@2x.png" alt="Fondation Abdelouahed EL KADIRI" /></a>
+        <a Link="/"><img src="\uploads\logo@2x.png" alt="Fondation Abdelouahed EL KADIRI" /></a>
       </div>
       <div className="menu">
         <ul>
