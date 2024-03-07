cde4081ae318736f7316704ecd1e7f75c397b49c
Wizardy <wizardhassan99@gmail.com>
2024-03-08 00:26:31 +0100

    commit

--- src/components/projets-components/auditorium/Auditorium.jsx
+++ src/components/projets-components/auditorium/Auditorium.jsx
@@ -4,7 +4,21 @@ import './auditorium.scss'
 
 function Auditorium() {
   return (
-    <div>Auditorium</div>
+    <div className="auditorium">
+      <div className="left">
+        <img src="\uploads\auditorium2.jpg" alt="Auditorium" />
+      </div>
+      <div className="right">
+        <h2>Auditorium</h2>
+        <hr />
+        <p>
+          Un lieu dédié à la création et à la rencontre entre les musiciens et les artistes de toutes disciplines. L’Auditorium offre une expérience hors du commun à ses visiteurs grâce à son acoustique exceptionnelle et ses équipements innovants.
+        </p>
+        <p>
+          Afin de refléter la diversité et vitalité du monde de la musique, la Fondation propose dans l’Auditorium des concerts classiques, jazz ou contemporains... Depuis son inauguration, l’Auditorium a accueilli de nombreux artistes d’envergure internationale .
+        </p>
+      </div>
+    </div>
   )
 }
 
