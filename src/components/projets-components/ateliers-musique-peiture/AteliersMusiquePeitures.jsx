2b04acbceda5c6da431e2fe044da4d001514dfad
Wizardy <wizardhassan99@gmail.com>
2024-03-08 00:26:22 +0100

    commit

--- src/components/projets-components/ateliers-musique-peiture/AteliersMusiquePeitures.jsx
+++ src/components/projets-components/ateliers-musique-peiture/AteliersMusiquePeitures.jsx
@@ -4,7 +4,24 @@ import "./ateliers-musique-peiture.scss"
 
 function AteliersMusiquePeitures() {
   return (
-    <div>AteliersMusiquePeitures</div>
+    <div className="ateliers-musique-peiture">
+      <div className="left">
+        <img src="\uploads\musique-peinture.jpg" alt="Ateliers de musique / peinture" />
+      </div>
+      <div className="right">
+        <h2>Ateliers de musique / peinture</h2>
+          <hr />
+          <p>
+            Le Complexe Culturel Abdelouahed El Kadiri vient répondre à la volonté de Sa Majesté Le Roi Mohammed VI, que Dieu le glorifie, de faire de l'élément culturel un levier du développement territorial et du rayonnement des villes un pole culturel polyvalent qui aura pour mission la promotion de l'activité artistique dans la ville d'El Jadida, (cinéma, musique, théâtre, etc) et la symbiose des générations de différentes catégories sociales et culturelles. Le projet trouve son inspiration dans une lecture des éléments constitutifs des paysages naturels et urbains.
+          </p>
+          <p>
+            Structurer l'espace comprend donc l'assemblage de matériaux et de moyens techniques pour créer des espaces.
+          </p>
+          <p>
+            Sur une surface totale de plancher de 5 590 m² le Complexe Culturel Abdelouahed El Kadiri se veut un pôle privilegie du développement culturel de la région d'El Jalida.
+          </p>
+      </div>
+    </div>
   )
 }
 
